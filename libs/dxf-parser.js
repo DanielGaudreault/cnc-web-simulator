0fa01dabb017bec1c346343035788e55c826c928
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2025-04-03 13:17:25 -0400

    Update dxf-parser.js

--- libs/dxf-parser.js
+++ libs/dxf-parser.js
@@ -1,1 +1,11 @@
+import DxfParser from 'dxf-parser';
 
+// Grab fileText in node.js or browser
+const fileText = ...;
+
+const parser = new DxfParser();
+try {
+    const dxf = parser.parse(fileText);
+} catch(err) {
+    return console.error(err.stack);
+}
