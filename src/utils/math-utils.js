31c0a2902eb1dd2ba6e509c49655e71711e86406
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2025-04-08 12:04:19 -0400

    Update math-utils.js

--- src/utils/math-utils.js
+++ src/utils/math-utils.js
@@ -1,1 +1,55 @@
+class MathUtils {
+    static degToRad(degrees) {
+        return degrees * (Math.PI / 180);
+    }
 
+    static radToDeg(radians) {
+        return radians * (180 / Math.PI);
+    }
+
+    static clamp(value, min, max) {
+        return Math.min(Math.max(value, min), max);
+    }
+
+    static lerp(start, end, t) {
+        return start + (end - start) * t;
+    }
+
+    static distance(p1, p2) {
+        const dx = p2.x - p1.x;
+        const dy = p2.y - p1.y;
+        const dz = p2.z - p1.z;
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    static normalizeVector(v) {
+        const length = Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+        return {
+            x: v.x / length,
+            y: v.y / length,
+            z: v.z / length
+        };
+    }
+
+    static crossProduct(v1, v2) {
+        return {
+            x: v1.y * v2.z - v1.z * v2.y,
+            y: v1.z * v2.x - v1.x * v2.z,
+            z: v1.x * v2.y - v1.y * v2.x
+        };
+    }
+
+    static pointInPolygon(point, polygon) {
+        // Ray-casting algorithm
+        let inside = false;
+        for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+            const xi = polygon[i].x, yi = polygon[i].y;
+            const xj = polygon[j].x, yj = polygon[j].y;
+            
+            const intersect = ((yi > point.y) !== (yj > point.y))
+                && (point.x < (xj - xi) * (point.y - yi) / (yj - yi) + xi);
+            if (intersect) inside = !inside;
+        }
+        return inside;
+    }
+}
