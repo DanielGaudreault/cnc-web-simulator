8bc016209a330b169e6aa8bb8e3ea064279d1ae0
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2025-04-08 11:48:12 -0400

    Update command-manager.js

--- src/ui/command-manager.js
+++ src/ui/command-manager.js
@@ -1,1 +1,59 @@
+class CommandManager {
+    constructor() {
+        this.commandStack = [];
+        this.undoStack = [];
+        this.maxHistory = 100;
+    }
 
+    execute(command) {
+        command.execute();
+        this.commandStack.push(command);
+        
+        // Truncate if too long
+        if (this.commandStack.length > this.maxHistory) {
+            this.commandStack.shift();
+        }
+        
+        // Clear redo stack when new command is executed
+        this.undoStack = [];
+    }
+
+    undo() {
+        if (this.commandStack.length === 0) return;
+        
+        const command = this.commandStack.pop();
+        command.undo();
+        this.undoStack.push(command);
+    }
+
+    redo() {
+        if (this.undoStack.length === 0) return;
+        
+        const command = this.undoStack.pop();
+        command.execute();
+        this.commandStack.push(command);
+    }
+
+    clear() {
+        this.commandStack = [];
+        this.undoStack = [];
+    }
+}
+
+class CreateBoxCommand {
+    constructor(cadEngine, params) {
+        this.cadEngine = cadEngine;
+        this.params = params;
+        this.createdObject = null;
+    }
+
+    execute() {
+        this.createdObject = this.cadEngine.createPrimitive('box', this.params);
+    }
+
+    undo() {
+        if (this.createdObject) {
+            this.cadEngine.removeObject(this.createdObject.id);
+        }
+    }
+}
