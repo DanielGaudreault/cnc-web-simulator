50ea4a8f690ad486e58b1248d336e7bda43704f1
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2025-04-08 13:51:26 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,20 @@
+class WorkerHandler {
+    constructor(workerScript) {
+        this.worker = new Worker(workerScript);
+    }
+
+    postMessage(task, payload) {
+        return new Promise((resolve, reject) => {
+            this.worker.onmessage = (event) => resolve(event.data);
+            this.worker.onerror = (error) => reject(error);
+            this.worker.postMessage({ task, payload });
+        });
+    }
+
+    terminate() {
+        this.worker.terminate();
+    }
+}
+
+export default WorkerHandler;
 
